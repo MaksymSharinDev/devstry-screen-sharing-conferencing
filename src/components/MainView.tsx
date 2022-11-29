47da4d335fad3d17fd103a2ae4e96d45cb4f0393
Maksym Sharin <Maksym.sharin.work@gmail.com>
2022-11-29 01:11:40 +0100

    poc: mainView use machine

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,13 +1,41 @@
-// react component
+
+import { useMachine } from '@xstate/react';
+import { useEffect, useRef } from 'react';
+
+import PresenterOverlay from './organisms/PresenterOverlay';
+import Connecting from './atoms/Connecting';
+
+import appMachine from '../actors/App/AppMachine';
+import { ROLE } from '../sdk/enums';
+
 
 const MainView = () => {
 
-    return (
-        <div>
-            <h1>Main View</h1>
-        </div>
 
+    const [state, transition, interpreter] = useMachine(appMachine,{
+        devTools: true,
+    });
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const initCanvas = () => { canvasRef.current?.getContext('2d')?.clearRect(0, 0, 1000, 1000); };
+
+    useEffect(() => {
+        initCanvas();
+    }, []);
+
+
+    return (<>
+        {state.matches('Connecting') &&
+            <Connecting />
+        }
+        <canvas ref={canvasRef} className="w-100 h-100" />
+        {state.matches('ConferenceView') &&
+            state.context.role === ROLE.PRESENTER &&
+            <PresenterOverlay />
+        }
+    </>
     );
-};
+}
+
 
 export default MainView;
